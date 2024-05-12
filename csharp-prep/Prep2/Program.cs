65ededcfff8e2a21cee718e55df1e11f38fe6e9b
estebanmontero <estebanmontero@hotmail.es>
2024-05-12 00:48:10 -0500

    commit 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,77 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Welcome usur to my  Grade Calculator!");
+
+        int percent = GetValidGradePercentageFromUser();
+
+        string letter = CalculateLetterGrade(percent);
+
+        DisplayResults(letter, percent);
+    }
+
+    static int GetValidGradePercentageFromUser()
+    {
+        int percent;
+        bool isValidInput = false;
+
+        do
+        {
+            Console.Write("what is your  grade percentage be(0-100): ");
+            string input = Console.ReadLine();
+
+            isValidInput = int.TryParse(input, out percent);
+
+            if (!isValidInput || percent < 0 || percent > 100)
+            {
+                Console.WriteLine("Invalid input! Please enter a number between 0 and 100.");
+                isValidInput = false;
+            }
+
+        } while (!isValidInput);
+
+        return percent;
+    }
+
+    static string CalculateLetterGrade(int percent)
+    {
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        return letter;
+    }
+
+    static void DisplayResults(string letter, int percent)
+    {
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass this time. Better luck next time!");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        Console.WriteLine("Welcome to the Number Guessing Game! I've chosen a number between 1 and 100.");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Take a guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Oops, too low.");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Hmm, too high.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations, you guessed it!");
+            }
+
+        } 
+
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
