ca509ba9e7fedd8a53fc0b8d9dfbde4b5efdf91c
estebanmontero <estebanmontero@hotmail.es>
2024-06-19 17:33:07 -0500

    commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,42 +1,9 @@
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers = new List<int>();
-
-        while (true)
-        {
-            Console.Write("Enter a number (0 to quit): ");
-            int userNumber = int.Parse(Console.ReadLine());
-
-            if (userNumber == 0)
-                break;
-
-            numbers.Add(userNumber);
-        }
-
-        int sum = 0;
-        foreach (int number in numbers)
-        {
-            sum += number;
-        }
-
-        float average = (float)sum / numbers.Count;
-        Console.WriteLine($"The sum is: {sum}");
-        Console.WriteLine($"The average is: {average}");
-
-        int max = numbers[0];
-        foreach (int number in numbers)
-        {
-            if (number > max)
-            {
-                max = number;
-            }
-        }
-
-        Console.WriteLine($"The max is: {max}");
+        Console.WriteLine("Hello Prep4 World!");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,28 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the program!");
-
-        Console.Write("Please enter your name: ");
-        string userName = Console.ReadLine();
-
-        Console.Write("Please enter your favorite number: ");
-        string userInput = Console.ReadLine();
-
-        int userNumber;
-        if (int.TryParse(userInput, out userNumber))
-        {
-            int squaredNumber = SquareNumber(userNumber);
-            Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
-        }
-        else
-        {
-            Console.WriteLine("Invalid input! Please enter a valid number next time.");
-        }
-    }
-
-    static int SquareNumber(int number)
-    {
-        return number * number;
+        Console.WriteLine("Hello Prep5 World!");
     }
 }
